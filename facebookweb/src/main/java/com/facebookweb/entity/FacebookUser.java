76f5c8d7ec00f950d4dde8709c1c3787671449f0
munee <munee@Muneeswar>
2022-05-20 14:42:58 +0530

    facebookweb

---
+++ facebookweb/src/main/java/com/facebookweb/entity/FacebookUser.java
@@ -0,0 +1,68 @@
+package com.facebookweb.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "facebookrevature12")
+public class FacebookUser {
+	@Column(length = 25, name="username")
+	private String name;
+	private String password;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long user_id;
+	
+	private String email;
+	private String address;
+	
+	@OneToMany(mappedBy = "fb")
+	private List<Order> order;
+	
+	
+	public long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+	public List<Order> getOrder() {
+		return order;
+	}
+	public void setOrder(List<Order> order) {
+		this.order = order;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
